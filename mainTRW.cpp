09cf3967a39bdad240f2905e13395bc7aaa747d7
Greggot <53041724+Greggot@users.noreply.github.com>
2020-11-18 13:28:59 +0300

    Delete mainTRW.cpp

--- mainTRW.cpp
+++
@@ -1,9 +0,0 @@
-#include "include/Parse.h"
-
-int main(int argc, char *argv[])
-{
-    if(ParserVolvo::FromTxtToBin(argv[1], argv[2]))   //Первый аргумент - путь к файлу.txt, Второй - путь к файлу.bin
-        exit(1);
-
-    return 0;
-}
